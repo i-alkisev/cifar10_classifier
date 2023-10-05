97a7905c040f85f52e3bc3163618680d1ffe0625
i-alkisev <i.alkisev@gmail.com>
2023-10-06 02:40:38 +0300

    Added evaluating model in infer.py

--- cifar10_classifier/infer.py
+++ cifar10_classifier/infer.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import torch
-from data import dataset_cifar10_test
+from data import dataset_cifar10_test, dataset_cifar10_train
 from model import ConvNet
+from train import eval_model
 
 
 def make_prediction(network, dataloader):
@@ -28,12 +29,24 @@ if __name__ == "__main__":
     dl_test = torch.utils.data.DataLoader(
         dataset_cifar10_test, batch_size=batch_size, num_workers=2
     )
+    dl_train = torch.utils.data.DataLoader(
+        dataset_cifar10_train, batch_size=batch_size, shuffle=True, num_workers=2
+    )
 
     model_filename = "cifar10_cnn_classifier.pth"
     print(f"Loading model from '{model_filename}'")
     conv_network = ConvNet(use_batchnorm=True)
     conv_network.load_state_dict(torch.load(model_filename))
     conv_network.to(device)
+
+    print("Evaluating model")
+    loss_fn = torch.nn.CrossEntropyLoss()
+    train_loss, train_accuracy = eval_model(conv_network, loss_fn, dl_train, device)
+    test_loss, test_accuracy = eval_model(conv_network, loss_fn, dl_test, device)
+    print(
+        f"Loss (Train/Test): {train_loss:.3f}/{test_loss:.3f}."
+        f" Accuracy, % (Train/Test): {train_accuracy:.2f}/{test_accuracy:.2f}"
+    )
 
     print("Making test predictions")
     prediction = make_prediction(conv_network, dl_test)
