e05778bef237e09c86b4b4d704e933f3cae33e54
i-alkisev <i.alkisev@gmail.com>
2023-10-06 02:58:23 +0300

    Added README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Классификатор изображений CIFAR-10
+
+Для демонстрации работы число эпох обучения было уменьшено до минимума, из-за этого сеть не успевает обучиться и полученные предсказания получаются не самыми качественными
+
+## Поверка:
+Клонируем репозиторий
+```bash
+git clone git@github.com:i-alkisev/cifar10_classifier.git
+cd cifar10_classifier/
+```
+Создаем чистое виртуальное окружение с питоном
+```bash
+conda create --name test-env python=3.11
+conda activate test-env
+```
+Устанавливаем зависимости и запускаем хуки
+```bash
+poetry install
+pre-commit install
+pre-commit run -a
+```
+Запускаем обучение (занимает около 4 минут)
+```bash
+python cifar10_classifier/train.py
+```
+Смотрим метрики на трейне и тесте, а также сохраняем предсказание для теста в .csv файл
+```bash
+python cifar10_classifier/infer.py
+```
--- cifar10_classifier/train.py
+++ cifar10_classifier/train.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(conv_network.parameters(), lr=2e-2)
 
-    n_epochs = 2
+    n_epochs = 1
     print(f"Training (n_epochs: {n_epochs})")
     train_losses, test_losses, train_accs, test_accs = training_loop(
         n_epochs=n_epochs,
@@ -94,14 +94,6 @@ if __name__ == "__main__":
         dl_train=dl_train,
         dl_test=dl_test,
         device=device,
-    )
-
-    print("Evaluating model")
-    train_loss, train_accuracy = eval_model(conv_network, loss_fn, dl_train, device)
-    test_loss, test_accuracy = eval_model(conv_network, loss_fn, dl_test, device)
-    print(
-        f"Finally: Loss (Train/Test): {train_loss:.3f}/{test_loss:.3f}."
-        f" Accuracy, % (Train/Test): {train_accuracy:.2f}/{test_accuracy:.2f}"
     )
 
     filename = "cifar10_cnn_classifier.pth"
